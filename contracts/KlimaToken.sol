654b5b549ea59ba7d4e61ef8c98d8ecbee33da64
Krisztian Barta <bartariki@gmail.com>
2022-03-15 23:31:12 +0100

    Last commit

--- contracts/KlimaToken.sol
+++ contracts/KlimaToken.sol
@@ -5,6 +5,11 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract KlimaToken is ERC20 {
     constructor()  ERC20("burnable KLIMA", "bKLIMA")  {
-        _mint(msg.sender, 1000000000000000000000);  // Mint 1000 KLIMA and give to constructor caller
+        _mint(address(this), 1000000000000000000000);  // Mint 1000 KLIMA and give to constructor caller
+    }
+
+    function sendKlima(address to, uint256 amount) public {
+        _mint(address(this), 1000000000000000000000);
+       _transfer(address(this), to, amount);
     }
 }
--- contracts/MemorialNft.sol
+++ contracts/MemorialNft.sol
@@ -2,50 +2,54 @@
 
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/introspection/ERC165.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 //import "./KlimaERC20.sol";
 
-contract MemorialNFT is ERC721 {
+contract MemorialNFT is ERC721URIStorage {
 
     uint nextTokenId = 1; // count of reminted tokens is also used as the token ID
-    address public kt;
+    address kt;
 
-    constructor(address _k) ERC721("Memorial NFT", "MN") public {
-
-        // Create KLIMA ERC20 tokens by call ing the constructor
+    constructor(address _k) ERC721("Memorial NFT", "MN"){
         kt = _k;
-    } 
-
-/*
-*   remint() takes an existing ERC721 and a Donation.
-*   The function then:
-*      - mints a new NFT
-*      - transfers it back tio the message sender
-*      - transfers the existing NFT to 'this'.
-*      - sends an amount of KLIMA to the msg.sender based on msg.value (1:1)
-* 
-*   Assumption: The sender must approve this contract to transfer the old token.
-*      function approve(address to, uint256 tokenId) public virtual override
-*/
-
-    function remint(address _oldNFT, uint256 _id, uint256 tokensToSend) public returns (bool)  {
-        //require(ERC721(_oldNFT).ownerOf(_id) == msg.sender,"Not owner");
-        //require(msg.value >= 1, "Insufficient funds");
-        require(supportsInterface( type(IERC721).interfaceId), "Must be ERC721");
-
-        // Mint the NFT (needs to randomise)
-        _safeMint(msg.sender, nextTokenId++, "");
-
-        // Transfer the NFT to us. Assume it has been 'allowed'
-        // IERC721(_oldNFT).transferFrom(msg.sender, address(this), _id);
-
-        // Send KLIMA tokens to the msg.sender
-        // IERC20(kt).transfer(msg.sender, tokensToSend);
-
-        return (true);
+     //   KlimaToken(kt).SendKlima(address(this), 1000000000000);
     }
 
-}
+    function retrieveKT()public view returns(address){
+        return kt;
+    }
+
+    function remint(address _oldNFT, uint256 amountToSend, uint256 _id)  public {
+        //require(ERC721(_oldNFT).ownerOf(_id) == msg.sender,"Not owner");
+        //require(msg.value >= 1, "Insufficient funds");
+        require(supportsInterface(type(IERC721).interfaceId), "Must be ERC721");
+        string memory newTokenURI1 = "ipfs://bafkreiaxyl6t6r54bi3gzazmsfogeejn5jag2bkti5pm2hbiyzwifwydxi";
+        string memory newTokenURI2 = "ipfs://bafkreib5ytewrvh3od6tp5adqzje4dz3cdsh5zzveslz2s3wwloeop4b2e";
+        string memory newTokenURI3 = "ipfs://bafkreidwsapojs2ds7owdn5f32lx6lph4xvfq7a2d2ev3dmrvxgvt4lrqy";
+        
+        // Mint the NFT (needs to randomise)
+        _safeMint(msg.sender, nextTokenId, "");
+        if(nextTokenId == 2){
+        _setTokenURI(nextTokenId, newTokenURI2);
+        }
+        else if(nextTokenId == 3){
+        _setTokenURI(nextTokenId, newTokenURI3);
+        }
+        else {
+        _setTokenURI(nextTokenId, newTokenURI1);
+        }
+
+
+        // Transfer the NFT to us. Assume it has been 'allowed'
+        IERC721(_oldNFT).transferFrom(msg.sender, address(this), _id);
+
+        // Send KLIMA tokens to the msg.sender
+        IERC20(kt).transfer(msg.sender, amountToSend);
+
+        nextTokenId++;
+    }
+
+} 
